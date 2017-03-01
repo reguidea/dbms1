2eac4c51130782b460d4b0f982d22ff5dd76613a
reguidea <latrellsalosagcol@gmail.com>
2017-03-01 10:15:34 +0800

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -12,11 +12,11 @@ alter user phpuser default tablespace users temporary tablespace temp account un
 drop user php_sec_admin cascade;
 create user php_sec_admin identified by welcome;
 alter user 	php_sec_admin default tablespace system temporary tablespace temp account unlock;
-grant create procedure, create session,	create table, resource select any dictionary to php_sec_admin;
+grant create procedure, create session,	create table, resource, select any dictionary to php_sec_admin;
 
 connect phpuser/welcome;
 --"Parts" table for the application demo	
-create table parts (id number primary key, category varchar2(20) name varchar2(20));
+create table parts (id number primary key, category varchar2(20), name varchar2(20));
 insert into parts values(1,'electrical','lamp');
 insert into parts values(2,'electrical','wire');
 insert into parts values(3,'electrical','switch');
